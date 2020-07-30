fe65bd14b659a2b69a4d4fec415769e67d4d1b55
unknown <onyenekwelizabeth@gmail.com>
2020-07-30 14:51:06 -0700

    done

--- components/Reddit/container.vue
+++ components/Reddit/container.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="bg-gray-200 h-screen overflow-y-scroll">
+    <div class="pt-8 text-center lg:hidden inline-block w-full px-3">
+      <input
+        v-model="searchterm"
+        class="bg-white rounded border border-gray-400 focus:outline-none focus:border-indigo-500 px-2 py-2 text-sm w-full border-t-0 border-r-0 border-l-0"
+        placeholder="Search"
+        type="text"
+        @input="search"
+      />
+    </div>
     <transition-group name="flip-list">
       <Post
         v-for="reddit in tweet"
@@ -19,6 +28,11 @@ export default {
     Post,
     Load,
   },
+  data() {
+    return {
+      searchterm: '',
+    }
+  },
   computed: {
     tweet() {
       return this.$store.state.data.Post
@@ -32,10 +46,15 @@ export default {
     await this.$store.dispatch('data/getIncidents')
     this.$store.commit('spin/loading', false)
   },
+  methods: {
+    search() {
+      this.$store.commit('data/duplicateClone', this.searchterm)
+    },
+  },
 }
 </script>
-<style scoped>
+<style>
 .flip-list-move {
-  transition: all transform 0.5s;
+  transition: transform 1s;
 }
 </style>
--- components/Reddit/sidebar.vue
+++ components/Reddit/sidebar.vue
@@ -6,7 +6,7 @@
           <img src="/img/reddit.png" class="w-8" alt="" />
         </div>
 
-        <div class="body-font pt-8 text-center">
+        <div class="ml-3 body-font pt-8 text-center lg:inline-block hidden">
           <input
             v-model="searchterm"
             class="bg-white rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2 border-t-0 border-r-0 border-l-0"
@@ -20,26 +20,29 @@
           <div>
             <button
               class="w-full text-left py-3 px-6 text-gray-600 cursor-pointer hover:bg-gray-100 hover:text-gray-700 focus:outline-none"
+              :class="active === 'Popular' ? 'changed' : ''"
               @click="popular()"
             >
-              <span class="mx-4 font-medium">Most Popular</span>
+              <span class="mx-4 font-medium">Popular</span>
             </button>
           </div>
 
           <div>
             <button
               class="w-full text-left py-3 px-6 text-gray-600 cursor-pointer hover:bg-gray-100 hover:text-gray-700 focus:outline-none"
+              :class="active === 'Unpopular' ? 'changed' : ''"
               @click="least()"
             >
-              <span class="mx-4 font-medium">Least Popular</span>
+              <span class="mx-4 font-medium">UnPopular</span>
             </button>
           </div>
           <div>
             <button
               class="w-full text-left py-3 px-6 text-gray-600 cursor-pointer hover:bg-gray-100 hover:text-gray-700 focus:outline-none"
+              :class="active === 'Recent' ? 'changed' : ''"
               @click="recent()"
             >
-              <span class="mx-4 font-medium">Recent Posts</span>
+              <span class="mx-4 font-medium">Recent</span>
             </button>
           </div>
           <div>
@@ -47,7 +50,11 @@
               class="w-full text-left py-3 px-6 text-gray-600 cursor-pointer hover:bg-gray-100 hover:text-gray-700 focus:outline-none"
               @click="old()"
             >
-              <span class="mx-4 font-medium">Old Posts</span>
+              <span
+                class="mx-4 font-medium"
+                :class="active === 'Old' ? 'changed' : ''"
+                >Old</span
+              >
             </button>
           </div>
         </nav>
@@ -59,61 +66,75 @@
         </div>
       </div>
     </div>
-    <div class="w-24 px-4 bg-white lg:hidden inline-block">
+    <div class="px-4 bg-white lg:hidden inline-block">
       <div class="w-full h-screen">
         <div class="flex items-center ml-2 pt-10 w-full">
           <img src="/img/reddit.png" class="w-8" alt="" />
         </div>
-        <div class="pt-8">
-          <input
-            v-model="searchterm"
-            class="bg-white rounded border border-gray-400 focus:outline-none focus:border-indigo-500 px-2 py-2 w-full text-sm border-t-0 border-r-0 border-l-0"
-            placeholder="Search"
-            type="text"
-            @input="search"
-          />
-        </div>
         <nav class="pt-10">
-          <div class="text-center">
+          <div
+            class="text-center"
+            :class="active === 'Popular' ? 'changed' : ''"
+            @click="popular()"
+          >
             <button
               class="flex ml-4 items-center py-3 text-gray-600 cursor-pointer hover:bg-gray-100 hover:text-gray-700 focus:outline-none"
-              @click="popular()"
             >
               <span>
                 <i class="fas fa-blog"></i>
               </span>
             </button>
+
+            <p class="text-xs">Popular</p>
           </div>
 
-          <div class="text-center">
+          <div
+            class="text-center"
+            :class="active === 'Unpopular' ? 'changed' : ''"
+            @click="least()"
+          >
             <button
               class="flex ml-4 items-center py-3 text-gray-600 cursor-pointer hover:bg-gray-100 hover:text-gray-700 focus:outline-none"
-              @click="least()"
             >
               <span>
                 <i class="fab fa-lastfm"></i>
               </span>
             </button>
+            <div>
+              <p class="text-xs">Unpopular</p>
+            </div>
           </div>
-          <div class="text-center">
+          <div
+            class="text-center"
+            :class="active === 'Recent' ? 'changed' : ''"
+            @click="recent()"
+          >
             <button
               class="flex ml-4 items-center py-3 text-gray-600 cursor-pointer hover:bg-gray-100 hover:text-gray-700 focus:outline-none"
-              @click="recent()"
             >
               <span>
                 <i class="far fa-newspaper"></i>
               </span>
             </button>
+            <div>
+              <p class="text-xs">Recent</p>
+            </div>
           </div>
-          <div class="text-center">
+          <div
+            class="text-center"
+            :class="active === 'Old' ? 'changed' : ''"
+            @click="old()"
+          >
             <button
               class="flex ml-4 items-center py-3 text-gray-600 cursor-pointer hover:bg-gray-100 hover:text-gray-700 focus:outline-none"
-              @click="old()"
             >
               <span>
                 <i class="fab fa-old-republic"></i>
               </span>
             </button>
+            <div>
+              <p class="text-xs">Old</p>
+            </div>
           </div>
         </nav>
 
@@ -129,6 +150,7 @@ export default {
   data() {
     return {
       searchterm: '',
+      active: '',
     }
   },
   methods: {
@@ -136,18 +158,26 @@ export default {
       this.$store.commit('data/duplicateClone', this.searchterm)
     },
     popular() {
+      this.active = 'Popular'
       this.$store.commit('data/moreVotes')
     },
     least() {
+      this.active = 'Unpopular'
       this.$store.commit('data/lessVotes')
     },
     recent() {
+      this.active = 'Recent'
       this.$store.commit('data/mostRecentPosts')
     },
     old() {
+      this.active = 'Old'
       this.$store.commit('data/leastRecentPosts')
     },
   },
 }
 </script>
-<style scoped></style>
+<style scoped>
+.changed {
+  color: red;
+}
+</style>
